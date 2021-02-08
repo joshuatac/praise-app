15f9e4657f4238eded6cfd8a08c21908dd306c45
joshuatac <4surejosh@gmail.com>
2021-02-08 15:22:38 +0100

    commit

---
+++ backend/data.js
@@ -0,0 +1,68 @@
+const data = {
+    products: [
+        {
+            _id: '1',
+            name: 'Hair drier',
+            category: 'fashion',
+            image: '/images/1.jpg',
+            price: 120,
+            countInStock: 10,
+            brand: 'nike',
+            rating: 2.5,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+
+        {
+            _id: '2',
+            name: 'Hanger',
+            category: 'fashion',
+            image: '/images/2.jpg',
+            price: 120,
+            countInStock: 0,
+            brand: 'nike',
+            rating: 5,
+            numReviews: 13,
+            description: 'high quality product'
+        },
+
+        {
+            _id: '3',
+            name: 'Hair drier',
+            category: 'fashion',
+            image: '/images/3.jpg',
+            price: 50,
+            countInStock: 0,
+            brand: 'nike',
+            rating: 2.5,
+            numReviews: 11,
+            description: 'high quality product'
+        },
+
+        {
+            _id: '4',
+            name: 'Hair drier',
+            category: 'fashion',
+            image: '/images/4.jpg',
+            price: 120,
+            countInStock: 10,
+            brand: 'nike',
+            rating: 1,
+            numReviews: 16,
+            description: 'high quality product'
+        }, {
+            _id: '5',
+            name: 'Hair drier',
+            category: 'fashion',
+            image: '/images/6.jpeg',
+            price: 120,
+            countInStock: 10,
+            brand: 'nike',
+            rating: 1.5,
+            numReviews: 40,
+            description: 'high quality product'
+        },
+    ]
+};
+
+export default data;
